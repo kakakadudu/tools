300fcb0224b965100f137b2a3ae3b038ae0aec9e
dybxdaaaaaa <dybxdaaaaaa@gmail.com>
2024-02-09 16:50:20 +0800

    update

--- tools.js
+++ tools.js
@@ -1,6 +1,7 @@
 class Tools {
   /**
-   * param {Function} fn 要执行的函数
+   * 函数柯里化
+   * @param {Function} fn 要执行的函数
    * @returns {Function}
    */
   static curry = function () {
@@ -22,4 +23,51 @@ class Tools {
     }
     return _curry;
   }
+
+  /**
+   * 函数防抖
+   * @param {Function} cb 回调函数
+   * @param {Number} duration 延迟时间
+   */
+  static debounce = function (cb, duration) {
+    var timer = null;
+    return function () {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        cb.apply(this, arguments);
+        timer = null;
+      }, duration)
+    }
+  }
+
+  /**
+   * 函数节流
+   * @param {*} cb 回调函数
+   * @param {*} duration 间隔时间
+   * @param {*} immediately 是否立即执行
+   */
+  static throttle = function (cb, duration, immediately) {
+    var time, timer = null;
+    if (immediately) {
+      return function () {
+        if (!time || Date.now() - time >= duration) {
+          cb.apply(this, arguments);
+          time = Date.now();
+        }
+      }
+    } else {
+      return function () {
+        if (timer) {
+          return;
+        }
+        timer = setTimeout(() => {
+          cb.apply(this, arguments);
+          timer = null;
+        }, duration)
+      }
+    }
+  }
 }
+
